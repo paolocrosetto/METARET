d863f0ba2702494e14c459a8e10158930be84d31
paolocrosetto <paolo.crosetto@gmail.com>
2019-08-27 15:24:33 +0200

    solved bug with r computation and task names in CroFil ExEc

--- Data/Crosetto_Filippin_Experimental_Economics_2016/clean_data.R
+++ Data/Crosetto_Filippin_Experimental_Economics_2016/clean_data.R
@@ -53,25 +53,25 @@ df <- df %>%
 
 
 ## Computing the CRRA (x^r) coefficient of risk aversion from the task data
-df <- df %>% mutate(r = case_when(task == "BRET" ~ choice/(100-choice),
-                                  task == "Investment Game" & choice != 4 ~ (log(4-choice) - log(8+3*choice) + log(3))/(log(4-choice) +log(2) - log(8+3*choice)),
-                                  task == "Investment Game" & choice == 4 ~ 1,
-                                  task == "Binswanger" & choice == 1 ~ -1,
-                                  task == "Binswanger" & choice == 2 ~ (-1+0.33)/2,
-                                  task == "Binswanger" & choice == 3 ~ (0.33+0.62)/2,
-                                  task == "Binswanger" & choice == 4 ~ (0.62+0.8)/2,
-                                  task == "Binswanger" & choice == 5 ~ 1,
-                                  task == "Holt & Laury" & choice == 0 ~ 1.95,
-                                  task == "Holt & Laury" & choice == 1 ~ 1.95,
-                                  task == "Holt & Laury" & choice == 2 ~ (1.49+1.95)/2,
-                                  task == "Holt & Laury" & choice == 3 ~ (1.49+1.15)/2,
-                                  task == "Holt & Laury" & choice == 4 ~ (0.85+1.15)/2,
-                                  task == "Holt & Laury" & choice == 5 ~ (0.59+0.85)/2,
-                                  task == "Holt & Laury" & choice == 6 ~ (0.32+0.59)/2,
-                                  task == "Holt & Laury" & choice == 7 ~ (0.03+0.32)/2,
-                                  task == "Holt & Laury" & choice == 8 ~ (0.03+-0.37)/2,
-                                  task == "Holt & Laury" & choice == 9 ~ -0.37,
-                                  task == "Holt & Laury" & choice == 10 ~ -0.37))
+df <- df %>% mutate(r = case_when(task == "BART" ~ choice/(100-choice),
+                                  task == "IG" & choice != 4 ~ (log(4-choice) - log(8+3*choice) + log(3))/(log(4-choice) +log(2) - log(8+3*choice)),
+                                  task == "IG" & choice == 4 ~ 1,
+                                  task == "EG" & choice == 1 ~ -1,
+                                  task == "EG" & choice == 2 ~ (-1+0.33)/2,
+                                  task == "EG" & choice == 3 ~ (0.33+0.62)/2,
+                                  task == "EG" & choice == 4 ~ (0.62+0.8)/2,
+                                  task == "EG" & choice == 5 ~ 1,
+                                  task == "HL" & choice == 0 ~ 1.95,
+                                  task == "HL" & choice == 1 ~ 1.95,
+                                  task == "HL" & choice == 2 ~ (1.49+1.95)/2,
+                                  task == "HL" & choice == 3 ~ (1.49+1.15)/2,
+                                  task == "HL" & choice == 4 ~ (0.85+1.15)/2,
+                                  task == "HL" & choice == 5 ~ (0.59+0.85)/2,
+                                  task == "HL" & choice == 6 ~ (0.32+0.59)/2,
+                                  task == "HL" & choice == 7 ~ (0.03+0.32)/2,
+                                  task == "HL" & choice == 8 ~ (0.03+-0.37)/2,
+                                  task == "HL" & choice == 9 ~ -0.37,
+                                  task == "HL" & choice == 10 ~ -0.37))
 
 
 # Order of variables
