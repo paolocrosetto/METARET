210a7fc78bbdc7e92a7b28b10acf91517c139a63
paolocrosetto <paolo.crosetto@gmail.com>
2019-08-27 17:07:15 +0200

    further playing around with visualisation of the risk elicitation parameter across tasks

--- playground.R
+++ playground.R
@@ -12,7 +12,7 @@ df <- list.files(recursive = T, pattern = "^formatted_dataset") %>%
   map_dfr(read_csv)
 
 ## save the df to the shiny director for deployment
-df %>% write_csv("home/paolo/METARET/METARET_github/Shiny/METARET/df.csv")
+df %>% write_csv("/home/paolo/Dropbox/METARET/METARET_github/Shiny/METARET/df.csv")
 
 #### playground for the final plot -- commented away
 
@@ -46,7 +46,7 @@ ggsave("main_META_plot_playground.png")
 
 ## plot to visualize the 'r' parameter estimates from different tasks and treatments
 df %>% 
-  filter(bibkey == "Crosetto2016") %>% 
+  # filter(bibkey == "Crosetto2016") %>% 
   ggplot(aes(r, color = task, fill = task))+
   geom_density(alpha = 0.3, adjust = 0.2)+
   coord_cartesian(xlim = c(-1,2))+
@@ -54,11 +54,53 @@ df %>%
 
 library(ggridges)
 
+tasks <- c("IG","EG","HL",'BART',"BRET")
+tasks <- c("IG","EG")
+tasks <- "HL"
 df %>% 
+  filter(task %in% tasks) %>% 
   ggplot(aes(x = r, y = task, color = task, fill = task))+
-  geom_density_ridges(alpha = 0.3, bandwidth = 0.001, scale = 2)+
+  geom_density_ridges(alpha = 0.3, scale = 2, bandwidth = 0.1, from = -1.5, to = 2.5)+
   coord_cartesian(xlim = c(-1.5,2.5))+
   geom_vline(xintercept = 1, color = "red", linetype = "dashed")+
   labs(x = "risk aversion parameter r (CRRA, x^r)",
        y = "task")+
   theme(legend.title = element_blank())
+
+## raincloud plot
+source("flat_violin.R")
+library(ggbeeswarm)
+df %>% 
+  filter(r > -1.5 & r < 2.5) %>% 
+ggplot(aes(task, r, color=task, fill = task)) +
+  geom_flat_violin(position = position_nudge(x = 0.15, y = 0),
+                   alpha = 0.7, 
+                   adjust = 0.8) +
+  geom_boxplot(fill = 'white',
+               color = 'grey30',
+               width = 0.25,
+               outlier.alpha = 0) +
+  geom_point(alpha = 0.2,
+             position = position_jitter(width = 0.12, height = 0),
+             size = 2) +
+  coord_flip()
+
+## simple dot + whisker plot
+df %>% 
+  filter(r < 10) %>% 
+  group_by(task) %>% 
+  summarise(m = mean(r, na.rm = T),
+            sd = sd(r, na.rm = T),
+            se = sd/sqrt(n()),
+            ci = se * qt(.95/2 + .5, n()-1),
+            cih = m+ci,
+            cil = m-ci) %>% 
+  ggplot(aes(m, reorder(task,m), color = task, fill = task))+
+  geom_point(shape = 21, size = 3)+
+  geom_errorbarh(aes(xmin = cil, xmax = cih), height = 0.1)+
+  theme(legend.title = element_blank())
+  
+df %>% 
+  filter(task == "BRET") %>% 
+  ggplot(aes(r))+
+  geom_histogram()
