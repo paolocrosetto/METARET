d8b0fd68a05ae5ea1e26c7b9dd25563ff68d8656
paolocrosetto <paolo.crosetto@gmail.com>
2019-09-04 10:55:18 +0200

    Gnambs et al + Nielsen

--- Data/Nielsen_2019/clean_data.R
+++ Data/Nielsen_2019/clean_data.R
@@ -13,12 +13,31 @@ library(readxl)
 df <- read_dta("Data/Nielsen_2019/original_dataset.dta")
 
 ## some subjects have made no choice apparently (0 everywhere)
-df %>% 
-  filter(!(box1 == 0 & box2 == 0 & box3 == 0 & box4 ==0)) %>% 
-  mutate(treatment = as.factor(treatment)) %>% 
-  mutate(box1 = box1*4) %>% 
-  ggplot(aes(box1, color = treatment, fill = treatment))+
-  geom_histogram(alpha =0.5, position = position_dodge())
+df <- df %>% 
+  filter(!(box1 == 0 & box2 == 0 & box3 == 0 & box4 ==0))
+
+# scale choice up so that it is consistent with a 100-box BRET
+df <- df %>% 
+  mutate(subject = 345000 + row_number()) %>% 
+  select(subject, treatment, choice = box1) %>% 
+  mutate(choice = choice*4) %>% 
+  mutate(treatment = as.character(treatment)) %>% 
+  mutate(treatment = if_else(treatment == 1, "low stakes", "high stakes"))
+
+## Computing the CRRA (x^r) coefficient of risk aversion from the task data
+df <- df %>% 
+  mutate(r = choice/(100-choice)) %>% 
+  mutate(r = if_else(r == Inf, 99, r))
+  
+# Adding task, paper and bibtex handles
+df <- df %>% 
+  mutate(task = "BRET",
+         paper = "Nielsen, JEBO 2019",
+         bibkey = "Nielsen2019")
+
+# Order of variables
+df <- df %>% 
+  select(bibkey, paper, task, subject, choice, r, everything())
 
 # Writing to file
 df %>% write_csv("Data/Nielsen_2019/formatted_dataset.csv")
