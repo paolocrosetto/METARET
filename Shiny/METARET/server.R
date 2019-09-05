fc21df4594f40ba192b45f5476180f3c2c41b2bd
paolocrosetto <paolo.crosetto@gmail.com>
2019-09-06 01:11:09 +0200

    final slides for Dijon

--- Shiny/METARET/server.R
+++ Shiny/METARET/server.R
@@ -186,25 +186,25 @@
   acrossQuestPlot <- function(data){
     data %>% 
     ggplot(aes(reorder(subsample, m), choice)) +
-      geom_flat_violin(position = position_nudge(x = 0.15, y = 0),
-                       alpha = 0.7, 
-                       adjust = 0.8,
-                       scale = "width",
-                       fill = "grey70") +
-      geom_point(alpha = 0.4,
-                 position = position_jitter(width = 0.12, height = 0),
-                 size = 0.8, show.legend = F, 
-                 shape = 21,
-                 fill = "grey70") +  
-      geom_boxplot(fill = 'white',
-                   color = 'grey30',
-                   width = 0.25,
-                   outlier.alpha = 0,
-                   alpha = 0) +
+      # geom_flat_violin(position = position_nudge(x = 0.15, y = 0),
+      #                  alpha = 0.7, 
+      #                  adjust = 0.8,
+      #                  scale = "width",
+      #                  fill = "grey70") +
+      # geom_point(alpha = 0.4,
+      #            position = position_jitter(width = 0.12, height = 0),
+      #            size = 0.8, show.legend = F, 
+      #            shape = 21,
+      #            fill = "grey70") +  
+      # geom_boxplot(fill = 'white',
+      #              color = 'grey30',
+      #              width = 0.25,
+      #              outlier.alpha = 0,
+      #              alpha = 0) +
       # geom_errorbar(aes(ymin = cil, ymax = cih), width = 0.05, position = position_nudge(x = -0.2), show.legend = F)+
       # geom_point(aes(y = m), shape = 21, size = 2, position = position_nudge(x = -0.2), show.legend = F)+
       stat_summary(fun.data = mean_cl_boot, 
-                   geom = "pointrange", position = position_nudge(x = - 0.2, y = 0),
+                   geom = "pointrange", position = position_nudge(x = 0, y = 0),
                    show.legend = F)+
       # geom_hline(yintercept = 5, color = 'red', linetype = 'dashed', show.legend = F)+
       labs(y = "Likert scale answer",
