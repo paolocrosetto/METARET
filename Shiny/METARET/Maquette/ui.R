7d0db413dbd5825310d3b5b46be18e0e69f68d4a
JeanTavernier <jean.tavernier21@gmail.com>
2021-05-12 14:06:07 +0200

    nouvelle version horizontale

---
+++ Shiny/METARET/Maquette/ui.R
@@ -0,0 +1,81 @@
+library(shinydashboard)
+library(shiny)
+library(tidyverse)
+
+ui <- navbarPage(
+  theme = shinythemes::shinytheme("superhero"),
+  title = "METARET Data Explorer",
+  tabPanel("HOME", icon = icon("home"),
+           home_dashboard <- dashboardPage(
+             dashboardHeader(disable = TRUE),
+             dashboardSidebar(disable = TRUE),
+             dashboardBody()
+           )),
+  tabPanel("Tasks", icon = icon("list"),
+           navbarPage(
+             title = "Tasks Explorer:",
+             navbarMenu("Holt and Laury",
+                        tabPanel("Presentation", icon = icon("th"),
+                                 HL_dashboard1 <- dashboardPage(
+                                   dashboardHeader(disable = TRUE),
+                                   dashboardSidebar(disable = TRUE),
+                                   dashboardBody()
+                                 )),
+                        tabPanel("Analysis", icon = icon("line-chart"),
+                                 HL_dashboard2 <- dashboardPage(
+                                   dashboardHeader(disable = TRUE),
+                                   dashboardSidebar(disable = TRUE),
+                                   dashboardBody()
+                                 ))
+             ),
+             navbarMenu("Binswanger, Eckel and Grossmann",
+                        tabPanel("Presentation", icon = icon("th"),
+                                 BEG_dashboard1 <- dashboardPage(
+                                   dashboardHeader(disable = TRUE),
+                                   dashboardSidebar(disable = TRUE),
+                                   dashboardBody()
+                                 )),
+                        tabPanel("Analysis", icon = icon("line-chart"),
+                                 BEG_dashboard2 <- dashboardPage(
+                                   dashboardHeader(disable = TRUE),
+                                   dashboardSidebar(disable = TRUE),
+                                   dashboardBody()
+                                 ))
+           ),
+           navbarMenu("Investment Game", icon = icon("dollar"),
+                      tabPanel("Presentation", icon = icon("th"),
+                               IG_dashboard1 <- dashboardPage(
+                                 dashboardHeader(disable = TRUE),
+                                 dashboardSidebar(disable = TRUE),
+                                 dashboardBody()
+                               )),
+                      tabPanel("Analysis", icon = icon("line-chart"),
+                               IG_dashboard2 <- dashboardPage(
+                                 dashboardHeader(disable = TRUE),
+                                 dashboardSidebar(disable = TRUE),
+                                 dashboardBody()
+                               ))
+           ),
+           navbarMenu("Bomb Risk Elicitation Task", icon = icon("bomb"),
+                      tabPanel("Presentation", icon = icon("th"),
+                               bomb_dashboard1 <- dashboardPage(
+                                 dashboardHeader(disable = TRUE),
+                                 dashboardSidebar(disable = TRUE),
+                                 dashboardBody()
+                               )),
+                      tabPanel("Analysis", icon = icon("line-chart"),
+                               bombdashboard2 <- dashboardPage(
+                                 dashboardHeader(disable = TRUE),
+                                 dashboardSidebar(disable = TRUE),
+                                 dashboardBody()
+                               ))
+           )
+           
+           
+  )
+)
+)
+
+
+
+
