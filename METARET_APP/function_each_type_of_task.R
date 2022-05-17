bac29697579131e8471f676372a2450ff0cf80e7
Liza <54671871+golovanovaliza@users.noreply.github.com>
2022-05-17 15:47:27 +0200

    add new function for a graph across papers

---
+++ METARET_APP/function_each_type_of_task.R
@@ -0,0 +1,35 @@
+# density within versions of each task
+task_variability_plot <- function(selectedtask, data) {
+  
+  sem <- function(x, na.rm = FALSE) {
+    out <-sd(x, na.rm = na.rm)/sqrt(length(x))
+    return(out)
+  }
+  
+  data %>% 
+    group_by(paper)  %>%          # grouping variables
+    summarize(m = mean(r),          # calculating mean price
+              s = sem(r)) %>%    # calculating standard error
+    ggplot(aes(m, reorder(paper,m), group = paper)) + 
+    geom_point(color = 'red', size = 3) +                      # adding data points
+    geom_line() +                       # adding connecting lines
+    geom_errorbar(aes(xmin = m - s,     # adding lower error bars
+                      xmax = m + s))+  # adding upper error bars
+    labs(x = "Risk aversion parameter CRRA",
+         y = "")+
+    theme(legend.title = element_blank(),
+          legend.position = "none")+
+    scale_fill_brewer(palette = "Set1")+
+    scale_color_brewer(palette = "Set1") + theme_bw() +
+    #eliminates background, gridlines, and chart border
+    theme(
+      text = element_text(size=20),
+      plot.background = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.border = element_blank()
+    ) +
+    
+    #draws x and y axis line
+    theme(axis.line = element_line(color = 'black'))
+}
