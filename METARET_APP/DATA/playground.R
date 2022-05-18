88e66ebf576d1a4c68f630dcc6a93c550b1f06dd
Liza <54671871+golovanovaliza@users.noreply.github.com>
2022-05-18 20:06:04 +0200

    data optimisation

---
+++ METARET_APP/DATA/playground.R
@@ -0,0 +1,23 @@
+
+### playing around with the correlation plots
+library(tidyverse)
+library(broom)
+library(hrbrthemes)
+library(viridis)
+
+## getting data from all the "formatted_dataset.csv" files in each subdirectory of /Data
+df <- list.files(recursive = T, pattern = "^formatted_dataset") %>% 
+  map_dfr(read_csv)
+
+
+df = df %>% filter(task != 'quest_only') %>% 
+  full_join(df %>% filter(task == 'quest_only') %>% 
+              mutate(soep = choice,
+                     choice = NA)) 
+
+
+data=df %>% mutate(subject = paste0(bibkey, '_', subject))
+
+
+## save the df to the shiny director for deployment
+data %>% write_csv("DATA/df_mod.csv")
